203a090fee3e3741b7a04af85412885bab17f8d3
Eduarda Belles <117698621+heeybelles@users.noreply.github.com>
2025-12-03 14:27:14 -0300

    Update delete-user.php

--- delete-user.php
+++ delete-user.php
@@ -1,12 +1,20 @@
 <?php 
-  require 'config.php';
-   
-  if(isset($_GET['id'])){
-    $query = $pdo->prepare("DELETE FROM usuario WHERE id=?");
-    $query->execute([$_GET['id']]);
+require 'config.php';
 
-  }
+try {
+    if (isset($_GET['id'])) {
 
-   header("Location: list-users.php");
-   exit;
+        $id = $_GET['id'];
+
+        $pdo->prepare("DELETE FROM compras WHERE id_usuario = ?")->execute([$id]);
+
+        $pdo->prepare("DELETE FROM usuario WHERE id = ?")->execute([$id]);
+    }
+
+    header("Location: list-users.php");
+    exit;
+
+} catch (PDOException $e) {
+    echo "Erro ao excluir: " . $e->getMessage();
+}
 ?>
